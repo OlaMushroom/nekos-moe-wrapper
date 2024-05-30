192bab6ad5c7ffc6fc663e42bf7cc6ea48da71e2
Nyanto <ola.mushroom@gmail.com>
2024-05-30 23:52:41 +0700

    update

--- index.ts
+++ index.ts
@@ -1,26 +1,28 @@
 async function main(endpoint: string, options: object): Promise<any> {
-  const url = (new URL(endpoint, "https://nekos.moe/api/v1/")).toString();
+  const url = (new URL(endpoint, 'https://nekos.moe/api/v1/')).toString();
   console.log(url);
+
   try {
     const res = await (await fetch(url, options)).json();
     return res;
   } catch (err) {
-    throw Error("Error: ", { cause: err });;
+    throw Error('Error: ', { cause: err });
   }
 }
 
 export async function image(id: string): Promise<any> {
   return await main(`images/${id}`, {
-    method: "GET",
+    method: 'GET',
   });
 }
 
 export async function random(count?: number, nsfw?: boolean): Promise<any> {
-  let query = "?";
-  query = count !== undefined ? query + "count=" + count : query;
-  query = nsfw !== undefined ? query + "&nsfw=" + nsfw : query;
+  let query = '?';
+  query = count !== undefined ? query + 'count=' + count : query;
+  query = nsfw !== undefined ? query + '&nsfw=' + nsfw : query;
+
   return await main(`random/image${query}`, {
-    method: "GET",
+    method: 'GET',
   });
 }
 
@@ -36,8 +38,8 @@ export async function search(
   skip?: number,
   limit?: number
 ): Promise<any> {
-  return await main("images/search", {
-    method: "POST",
+  return await main('images/search', {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
@@ -53,7 +55,30 @@ export async function search(
       skip: skip,
       limit: limit
     })
-  })
+  });
+}
+
+export async function upload(
+  auth: string,
+  image: File,
+  tags: Array<string>,
+  nsfw: boolean,
+  artist?: string
+): Promise<any> {
+  const formData = new FormData();
+  formData.append("image", image);
+  formData.append("tags", tags.toString());
+  formData.append("nsfw", nsfw.toString());
+  if (artist !== undefined) formData.append("artist", artist);
+
+  return await main('images', {
+    method: 'POST',
+    headers: {
+      'Authorization': auth,
+      'Content-Type': 'multipart/form-data'
+    },
+    body: formData
+  });
 }
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Response#a_php_call
