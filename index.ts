4564cbe33016ddd74c261a93095e0e7f34a97f0e
Nyanto <ola.mushroom@gmail.com>
2024-05-31 23:04:42 +0700

    update

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+/** function main(endpoint: string, options: object): Promise<any> */
 async function main(endpoint: string, options: object): Promise<any> {
   const url = (new URL(endpoint, "https://nekos.moe/api/v1/")).toString();
   console.log(`URL: ${url}`);
@@ -9,6 +10,7 @@ async function main(endpoint: string, options: object): Promise<any> {
   }
 }
 
+/** function auth(_username: string, _password: string): Promise<any> */
 export async function auth(_username: string, _password: string): Promise<any> {
   return await main("auth", {
     method: "POST",
@@ -22,10 +24,12 @@ export async function auth(_username: string, _password: string): Promise<any> {
   });
 }
 
+/** function image(id: string): Promise<any> */
 export async function image(id: string): Promise<any> {
   return await main(`images/${id}`, {});
 }
 
+/** function random(count?: number, nsfw?: boolean): Promise<any> */
 export async function random(count?: number, nsfw?: boolean): Promise<any> {
   let query = "?";
   query = count !== undefined ? query + "count=" + count : query;
@@ -33,6 +37,7 @@ export async function random(count?: number, nsfw?: boolean): Promise<any> {
   return await main(`random/image${query}`, {});
 }
 
+/** function search(_body: object): Promise<any> */
 export async function search(_body: object): Promise<any> {
   return await main("images/search", {
     method: "POST",
@@ -43,6 +48,7 @@ export async function search(_body: object): Promise<any> {
   });
 }
 
+/** function upload(auth: string, image: File, tags: Array<string>, nsfw: boolean, artist?: string): Promise<any> */
 export async function upload(auth: string, image: File, tags: Array<string>, nsfw: boolean, artist?: string): Promise<any> {
   const formData = new FormData();
   formData.append("image", image);
