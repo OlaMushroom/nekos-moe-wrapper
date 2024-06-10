1a514063f697b392672fb73faeacdd358825c3dc
Nyanto <ola.mushroom@gmail.com>
2024-06-10 22:20:06 +0700

    update

--- src/user.ts
+++ src/user.ts
@@ -42,7 +42,7 @@ async function search(fields: object = {}): Promise<any> {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(fields)
-  })).images;
+  })).users;
 }
 
 export const user = { auth, get, search }
