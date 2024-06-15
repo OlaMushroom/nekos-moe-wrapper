75ebd99bf1e7ac57917cbc77650570550837e585
Nyanto <ola.mushroom@gmail.com>
2024-06-15 22:52:28 +0700

    update

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,33 @@
 import { expect, test, describe } from "vitest";
 import { Post, User } from '../src/index.ts';
 
-function getRandomIntInclusive(min: number, max: number) {
+function getRndIntIncl(min: number, max: number): number {
   const minCeiled = Math.ceil(min);
   return Math.floor(Math.random() * (Math.floor(max) - minCeiled + 1) + minCeiled);
 }
 
-// Test: Post.get() method
 describe('Post.get()', () => {
   test("Image's IDs are the same.", async () => {
     expect((await Post.get("ry7gPEpg7")).id).toBe("ry7gPEpg7");
   })
 });
 
-//Test: User.get() method
+const rndPostCnt = getRndIntIncl(1, 50);
+describe('Post.search()', () => {
+  test("Search counts are the same.", async () => {
+    expect((await Post.search({ limit: rndPostCnt})).length).toBe(rndPostCnt);
+  })
+});
+
 describe("User.get()", () => {
   test("User's IDs are the same.", async () => {
     expect((await User.get("BkCBy21se")).id).toBe("BkCBy21se");
+  });
+});
+
+const rndUserCnt = getRndIntIncl(1, 100);
+describe("User.search()", () => {
+  test("User counts are the same.", async () => {
+    expect((await User.search({ limit: rndUserCnt})).length).toBe(rndUserCnt);
   });
 });
