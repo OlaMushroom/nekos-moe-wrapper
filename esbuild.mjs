3bfe5969b09d2491eae55c362721be46d43b28d1
Nyanto <ola.mushroom@gmail.com>
2024-06-27 21:37:24 +0700

    switch to esbuild

---
+++ esbuild.mjs
@@ -0,0 +1,19 @@
+import { build } from 'esbuild';
+
+const defaultConfig = {
+  bundle: true,
+  packages: 'external',
+  platform: 'neutral',
+  format: 'esm',
+  target: ['es2022'],
+  entryPoints: ['./src/index.ts'],
+  outdir: 'build'
+};
+
+await build(defaultConfig);
+
+await build({
+  ...defaultConfig,
+  minifyWhitespace: true,
+  entryNames: '[dir]/[name].min'
+});
