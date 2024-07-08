2bb0c1db29b9ee37fec8c76d68440863b6c3441f
Nyanto <ola.mushroom@gmail.com>
2024-07-09 02:30:04 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import type { PostSearch, UserSearch, UploadForm } from './types.ts';
+
 interface PostData {
   id: string;
   approver?: {
@@ -49,10 +50,7 @@ async function errorHandler(res: Response) {
   throw Error(`HTTP Error: ${res.status} ${res.statusText}\n`, { cause: msg });
 }
 
-async function sendRequest(
-  endpoint: string,
-  options: object = {}
-): Promise<unknown> {
+async function sendRequest(endpoint: string, options: object = {}) {
   const url = new URL(endpoint, 'https://nekos.moe/api/v1/');
   console.log(`URL: ${url.toString()}\nTimestamp: ${Date.now()}`);
   try {
@@ -92,30 +90,31 @@ export function create(
 // Post/Image
 /** Get a `Post` using ID. */
 export async function getPost(id: string): Promise<PostData> {
-  const data = (await sendRequest(`images/${id}`)) as { image: PostData };
-  return data.image;
+  return (await sendRequest(`images/${id}`)).image;
 }
 
 /** Search for `Post`(s). */
 export async function searchPost(
   options: PostSearch = {}
 ): Promise<PostData[]> {
-  const data = (await sendRequest('images/search', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(options)
-  })) as { images: PostData[] };
-  return data.images;
+  return (
+    await sendRequest('images/search', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(options)
+    })
+  ).images;
 }
 
 /** Get random `Post`(s). */
 export async function random(count = 1, nsfw?: boolean): Promise<PostData[]> {
   const params = new URLSearchParams({ count: count.toString() });
   if (nsfw !== undefined) params.append('nsfw', nsfw.toString());
-  const data = (await sendRequest(
-    `random/image?count=${count}${nsfw !== undefined ? `&nsfw=${nsfw}` : ''}`
-  )) as { images: PostData[] };
-  return data.images;
+  return (
+    await sendRequest(
+      `random/image?count=${count}${nsfw !== undefined ? `&nsfw=${nsfw}` : ''}`
+    )
+  ).images;
 }
 
 /**
@@ -128,14 +127,14 @@ export async function upload(form: UploadForm): Promise<UploadData> {
   formData.append('nsfw', form.nsfw.toString());
   formData.append('tags', form.tags.toString());
   if (form.artist !== undefined) formData.append('artist', form.artist);
-  return (await sendRequest('images', {
+  return await sendRequest('images', {
     method: 'POST',
     headers: {
       Authorization: form.token,
       'Content-Type': 'multipart/form-data'
     },
     body: formData
-  })) as UploadData;
+  });
 }
 
 // User
@@ -146,22 +145,20 @@ export async function upload(form: UploadForm): Promise<UploadData> {
 export async function getUser(id: string, token?: string): Promise<UserData> {
   const headers = new Headers();
   if (token !== undefined) headers.append('Authorization', token);
-  const data = (await sendRequest(`user/${id}`, { headers })) as {
-    user: UserData;
-  };
-  return data.user;
+  return (await sendRequest(`user/${id}`, { headers })).user;
 }
 
 /** Search for `User`(s). */
 export async function searchUser(
   options: UserSearch = {}
 ): Promise<UserData[]> {
-  const data = (await sendRequest('users/search', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(options)
-  })) as { users: UserData[] };
-  return data.users;
+  return (
+    await sendRequest('users/search', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(options)
+    })
+  ).users;
 }
 
 // Auth
@@ -170,12 +167,13 @@ export async function auth(
   username: string,
   password: string
 ): Promise<string> {
-  const data = (await sendRequest('auth', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username, password })
-  })) as { token: string };
-  return data.token;
+  return (
+    await sendRequest('auth', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password })
+    })
+  ).token;
 }
 
 /**
