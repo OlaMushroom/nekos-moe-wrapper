299fb6a1b01ae7ec5ea8ec616cb01bdf29453239
Nyanto <ola.mushroom@gmail.com>
2024-07-18 16:26:50 +0700

    switch to cac

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { cac } from 'cac';
 import { getPost, getUser, random } from '../lib/index.js';
-import { sendRequest, writeImage } from './main.js';
+import { sendRequest, writeImage, removeDir } from './main.js';
 
-const program = new Command('nekos');
-program.version('0.6.4');
+const cli = cac('nekos');
+cli.version('0.6.4');
+cli.option('-c, --clean', 'Clean the cache directory');
 
-program
-  .command('post')
-  .description('Get a `Post` using ID.')
+cli
+  .command('post <id>', 'Get a Post using ID')
   .alias('p')
-  .argument('<id>')
-  .option('-w --write', 'Write image to disk.')
+  .option('-w, --write', 'Write image to disk')
   .action(async (id, options) => {
     const data = await getPost(id);
     console.log(data);
@@ -20,23 +19,19 @@ program
       writeImage(data.id, await sendRequest(`image/${data.id}`));
   });
 
-program
-  .command('user')
-  .description('Get a `User` using ID. ')
+cli
+  .command('user <id>', 'Get a `User` using ID')
   .alias('u')
-  .argument('<id>')
   .action(async (id) => {
     const data = await getUser(id);
     console.log(data);
     console.log(`URL: https://nekos.moe/user/${data.id}`);
   });
 
-program
-  .command('random')
-  .description('Get random `Post`(s).')
+cli
+  .command('random [count]', 'Get random Post(s)')
   .alias('r')
-  .argument('[count]')
-  .option('-w --write', 'Write image to disk.')
+  .option('-w, --write', 'Write image to disk')
   .action(async (count, options) => {
     const data = await random(count);
     console.log(data);
@@ -46,4 +41,11 @@ program
     }
   });
 
-program.parse();
+cli.help();
+cli.parse();
+
+const opts = cli.options;
+if (opts.clean) {
+  if (removeDir('./images')) console.log('Cleaned cache successfully.');
+  else console.log('No cache directory is found.');
+}
--- bin/main.js
+++ bin/main.js
@@ -1,4 +1,4 @@
-import { mkdirSync, writeFileSync } from 'node:fs';
+import { existsSync, mkdirSync, rmSync, writeFileSync } from 'node:fs';
 
 export async function sendRequest(endpoint) {
   const url = new URL(endpoint, 'https://nekos.moe/');
@@ -13,11 +13,23 @@ export async function sendRequest(endpoint) {
 export async function writeImage(name, res) {
   try {
     const file = `${name}.${res.headers.get('content-type')?.includes('image/png') ? 'png' : 'jpg'}`;
-    console.log(`Writing file...: ${file}`)
+    console.log(`Writing file...: ${file}`);
     mkdirSync('./images', { recursive: true });
     writeFileSync(`./images/${file}`, new Uint8Array(await res.arrayBuffer()));
     console.log(`File written successfully: ${file}`);
   } catch (e) {
     throw Error('Error: ', { cause: e });
   }
+}
+
+export function removeDir(dir) {
+  if (existsSync(dir)) {
+    try {
+      rmSync(dir, { recursive: true, force: true });
+      return true;
+    } catch (e) {
+      throw Error('Error: ', { cause: e });
+    }
+  }
+  return false;
 }
