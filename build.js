bc8e5927558894e52a1f9b0f609dc4f825441654
Nyanto <ola.mushroom@gmail.com>
2024-07-26 12:12:03 +0700

    update

--- build.js
+++ build.js
@@ -1,18 +1,6 @@
-import { build } from 'esbuild';
-import { dtsPlugin } from 'esbuild-plugin-d.ts';
-
-await build({
-  entryPoints: ['src/index.ts'],
+await Bun.build({
+  entrypoints: ['src/index.ts'],
   outdir: 'lib',
-  bundle: true,
-  minify: true,
-  packages: 'external',
-  platform: 'neutral',
   format: 'esm',
-  target: ['es2022'],
-  plugins: [
-    dtsPlugin({
-      experimentalBundling: true
-    })
-  ]
+  minify: true
 });
