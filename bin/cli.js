83fb7f4ad3cf718773a6bec93c70e6923bd14ff7
Nyanto <ola.mushroom@gmail.com>
2024-07-31 21:13:44 +0700

    small fix

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,7 @@ import { getPost, getUser, random } from '../lib/index.js';
 import { sendRequest, writeImage, removeDir } from './main.js';
 
 const cli = cac('nekos');
-cli.version('0.7.1');
+cli.version('0.7.2');
 cli.option('-c, --clean', 'Clean the cache directory');
 
 cli
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,59 +5,58 @@
  * import * as nekos from '@om/nekos-moe';
  * ```
  */
-export interface PostData {
-	id: string;
-	approver?: {
-		id: string;
-		username: string;
-	};
-	artist?: string;
-	comments?: [
-	];
-	createdAt: string;
-	favorites?: number;
-	likes?: number;
-	pending?: boolean;
-	nsfw: string;
-	tags: string[];
-	uploader: {
-		id: string;
-		username: string;
-	};
+interface PostData {
+    id: string;
+    approver?: {
+        id: string;
+        username: string;
+    };
+    artist?: string;
+    comments?: [];
+    createdAt: string;
+    favorites?: number;
+    likes?: number;
+    pending?: boolean;
+    nsfw: string;
+    tags: string[];
+    uploader: {
+        id: string;
+        username: string;
+    };
 }
-export interface UserData {
-	id: string;
-	createdAt: string;
-	email: string;
-	favorites?: string[];
-	favoritesReceived: number;
-	likes?: string[];
-	likesReceived: number;
-	roles: string[];
-	savedTags?: string[];
-	uploads: number;
-	username: string;
-	verified?: boolean;
+interface UserData {
+    id: string;
+    createdAt: string;
+    email: string;
+    favorites?: string[];
+    favoritesReceived: number;
+    likes?: string[];
+    likesReceived: number;
+    roles: string[];
+    savedTags?: string[];
+    uploads: number;
+    username: string;
+    verified?: boolean;
 }
 /**
  * Creates a new image using `File` object.
  * @remarks File name is without file extension.
  */
-export declare function create(file: BlobPart, name: string, type: "jpeg" | "png"): File;
+export declare function create(file: BlobPart, name: string, type: 'jpeg' | 'png'): File;
 /** Get a `Post` using ID. */
 export declare function getPost(id: string): Promise<PostData>;
 /** Search for `Post`(s). */
 export declare function searchPost(options?: {
-	id?: string;
-	nsfw?: boolean;
-	uploader?: string | object;
-	artist?: string;
-	tags?: string[];
-	sort?: "newest" | "oldest" | "likes" | "relevance";
-	posted_before?: number;
-	posted_after?: number;
-	skip?: number;
-	limit?: number;
+    id?: string;
+    nsfw?: boolean;
+    uploader?: string | object;
+    artist?: string;
+    tags?: string[];
+    sort?: 'newest' | 'oldest' | 'likes' | 'relevance';
+    posted_before?: number;
+    posted_after?: number;
+    skip?: number;
+    limit?: number;
 }): Promise<PostData[]>;
 /** Get random `Post`(s). */
 export declare function random(count?: number, nsfw?: boolean): Promise<PostData[]>;
@@ -66,15 +65,15 @@ export declare function random(count?: number, nsfw?: boolean): Promise<PostData
  * @remarks This function uses the `FormData` object and set `'Content-Type': 'multipart/form-data'` to send the data.
  */
 export declare function upload(form: {
-	image: File;
-	artist?: string;
-	nsfw: boolean;
-	tags: string[];
-	token: string;
+    image: File;
+    artist?: string;
+    nsfw: boolean;
+    tags: string[];
+    token: string;
 }): Promise<{
-	image: PostData;
-	image_url: string;
-	post_url: string;
+    image: PostData;
+    image_url: string;
+    post_url: string;
 }>;
 /**
  * Get a `User` using ID.
@@ -83,9 +82,9 @@ export declare function upload(form: {
 export declare function getUser(id: string, token?: string): Promise<UserData>;
 /** Search for `User`(s). */
 export declare function searchUser(options?: {
-	query?: string;
-	skip?: number;
-	limit?: number;
+    query?: string;
+    skip?: number;
+    limit?: number;
 }): Promise<UserData[]>;
 /** Get token. */
 export declare function auth(username: string, password: string): Promise<string>;
@@ -94,5 +93,4 @@ export declare function auth(username: string, password: string): Promise<string
  * @remarks The new token will not be returned.
  */
 export declare function regen(token: string): Promise<void>;
-
 export {};
